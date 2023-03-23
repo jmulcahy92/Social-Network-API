025418c322148126946e5b09074b5f87108fd89d
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-03-23 00:15:34 -0500

    add & remove friends; user routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -63,4 +63,38 @@ module.exports = {
             return res.status(500).json(err);
         });
     },
+
+    addFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $addToSet: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    },
+
+    removeFriend(req, res) {
+        User.findOneAndUpdate(
+            { _id: req.params.userId },
+            { $pull: { friends: req.params.friendId } },
+            { runValidators: true, new: true }
+        )
+        .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID' })
+                : res.json(user)
+        )
+        .catch((err) => {
+            console.log(err);
+            return res.status(500).json(err);
+        });
+    }
 };
